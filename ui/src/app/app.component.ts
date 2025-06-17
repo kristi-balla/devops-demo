0b57ab1c23e678441b03142406c938635c2bc23e
janmalix <74460363+janmalix@users.noreply.github.com>
2025-06-17 10:45:38 +0200

    :rocket: chore(ui): add npm audit workflow (#27)
    * chore(ui): add Angular starter
    
    * chore(ci): add npm-audit action
    
    * chore(ui): add vulnerable dependency
    
    * chore(docs): add responsible for UI
    
    * chore(docker): containerize frontend
    
    * chore(spell): add words for ui
    
    * chore(spell): jan verewigen
    
    ---------
    
    Co-authored-by: Tesch, Jan (FT RPD CST SEA-DE) <jan.tesch@siemens.com>
    Co-authored-by: Kristi Balla <kristi.balla@siemens.com>

---
+++ ui/src/app/app.component.ts
@@ -0,0 +1,24 @@
+/*
+ * Copyright © Siemens 2025 - 2025
+ *
+ * Licensed under the Siemens Inner Source License 1.5
+ *
+ * Authors: Tesch, Jan (FT RPD CST SEA-DE)
+ *
+ * SPDX-FileCopyrightText: 2025 Siemens
+ *
+ * SPDX-License-Identifier: LicenseRef-Siemens-ISL-1.5
+ */
+
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ui';
+}
---
+++ ui/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright © Siemens 2025 - 2025
+ *
+ * Licensed under the Siemens Inner Source License 1.5
+ *
+ * Authors: Tesch, Jan (FT RPD CST SEA-DE)
+ *
+ * SPDX-FileCopyrightText: 2025 Siemens
+ *
+ * SPDX-License-Identifier: LicenseRef-Siemens-ISL-1.5
+ */
+
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [];
